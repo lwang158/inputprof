66783fcde32c67aeb761105a31633a47c570a742
lwang158 <lzw158@ist.psu.edu>
2017-12-05 18:47:00 -0500

    update

--- hashmap.h
+++ hashmap.h
@@ -1,11 +1,24 @@
+#define MAXNUM 268435456; //max number of hashmap elements
+#define MAP_MISS -3;
+#define MAP_FULL -2;
+#define MAP_EXCEED -1;
+#define MAP_SUCCESS 0;
+
 typedef struct _hashmap {
 	char * key;
-	int timestamp;
+	int value;
 	hashmap * next;
 }hashmap;
 
-void createhashmap(hashmap * hmap, int size);
+typedef struct _hashmapelem {
+	hashmap element[MAXNUM];
+	unsigned int n; // current size of hashmap
+} hashmapelem;
+
+extern hashmap * newhashmap();
+
+hashmap * createhashmap();
 void destroyhashmap(hashmap * hmap);
-int hashmapput(hashmap * hmap, char * key, int timestamp);
-int hashmapget(hashmap * hmap, char * key, int timestamp);
+int hashmapput(hashmap * hmap, char * key, int value);
+int hashmapget(hashmap * hmap, char * key, int value);
 
